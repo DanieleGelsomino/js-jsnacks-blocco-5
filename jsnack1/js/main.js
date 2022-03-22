5103477f5e164777c944f6c5f85915392bb559db
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-22 16:11:05 +0100

    add jsnack1 done

--- jsnack1/js/main.js
+++ jsnack1/js/main.js
@@ -6,3 +6,67 @@ console.log("JS OK!");
     indicando per ognuna varietà, peso e lunghezza.
     Calcola quanto pesano tutte le zucchine.
 */
+
+const zucchine = [
+  {
+    varietà: "chiara",
+    peso: 5,
+    lunghezza: 15,
+  },
+  {
+    varietà: "ortolano di Faenza",
+    peso: 5,
+    lunghezza: 14,
+  },
+  {
+    varietà: "fiorentina",
+    peso: 5,
+    lunghezza: 12,
+  },
+  {
+    varietà: "siciliana",
+    peso: 5,
+    lunghezza: 11,
+  },
+  {
+    varietà: "bianca triestina",
+    peso: 5,
+    lunghezza: 10,
+  },
+  {
+    varietà: "rigata pugliese",
+    peso: 5,
+    lunghezza: 9,
+  },
+  {
+    varietà: "romanesca",
+    peso: 5,
+    lunghezza: 8,
+  },
+  {
+    varietà: "tonda di Piacenza",
+    peso: 5,
+    lunghezza: 7,
+  },
+  {
+    varietà: "trombetta",
+    peso: 5,
+    lunghezza: 6,
+  },
+  {
+    varietà: "gialla",
+    peso: 5,
+    lunghezza: 5,
+  },
+];
+
+function sumPlants(vegetableFruits) {
+  let somma = 0;
+  for (let i = 0; i < vegetableFruits.length; i++) {
+    somma += vegetableFruits[i].peso;
+  }
+  return somma;
+}
+
+const sommaTotaleZucchine = sumPlants(zucchine);
+console.log(sommaTotaleZucchine);
